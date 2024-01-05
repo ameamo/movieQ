c6e3589b815d361ec5e494cd423e05c9f2dfb17f
ameamo <tatsuya070809@gmail.com>
2024-01-05 12:27:54 +0900

    update logo

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -7,7 +7,7 @@
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('homepage') }}">
                         <!-- <x-application-logo class="block h-9 w-auto fill-current text-gray-800" /> -->
-                        <img src="{{ asset('storage/MovieQロゴre.png') }}" width="150" height="80">
+                        <img src="{{ asset('img/MovieQロゴre.png') }}" width="150" height="80" alt="movieQロゴ">
                     </a>
                 </div>
 
